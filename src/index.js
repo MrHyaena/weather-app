3582ba39cbe5650bb2571d56a897f4037fc17f67
MrHyaena <dolezalmartin131@gmail.com>
2024-10-31 19:01:13 +0100

    Basic HTML, CSS and data fetch from API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import "./styles.css";
+
+async function getData() {
+  let city = "London";
+  let firstPart =
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+  let lastPart =
+    "/next7days?unitGroup=metric&include=hours&key=5EGSHHEJZZXCSVQR6AMZEE5Z7&contentType=json";
+  let apiUrl = firstPart + city + lastPart;
+  const response = await fetch(apiUrl, { mode: "cors" });
+  const dataJson = await response.json();
+  console.log(dataJson);
+}
+
+getData();
