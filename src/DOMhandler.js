10607ce811e4e9ed824a41e576674b83ab0291ed
MrHyaena <dolezalmartin131@gmail.com>
2024-11-02 10:19:09 +0100

    DOM element buttons, CSS styling

--- src/DOMhandler.js
+++ src/DOMhandler.js
@@ -5,22 +5,77 @@ export function createDom() {
   const city = document.querySelector("#city");
   let cityValue = "Prague";
   city.value = "Prague";
-  wholeProcess(cityValue);
+  let elementIndex = 0;
+  setTemperature(cityValue);
 
   showCity.addEventListener("click", () => {
+    elementIndex = 0;
     cityValue = city.value;
-    wholeProcess(cityValue);
+    setTemperature(cityValue);
+  });
+
+  const tempBtn = document.querySelector("#temp");
+  const windBtn = document.querySelector("#wind");
+  const wetBtn = document.querySelector("#wet");
+  const pressBtn = document.querySelector("#press");
+
+  tempBtn.addEventListener("click", () => {
+    elementIndex = 0;
+    setTemperature(cityValue);
+  });
+
+  windBtn.addEventListener("click", () => {
+    elementIndex = 1;
+    setWind(cityValue);
+  });
+
+  wetBtn.addEventListener("click", () => {
+    elementIndex = 2;
+    setHum(cityValue);
+  });
+
+  pressBtn.addEventListener("click", () => {
+    elementIndex = 3;
+    setPress(cityValue);
   });
 }
 
-function wholeProcess(cityValue) {
+function setTemperature(cityValue) {
   getData(cityValue)
-    .then((data) => changeDomData(data))
-    .then((data) => changeDomDay(data));
+    .then((data) => changeDisplayData(data))
+    .then((data) => changeDomDay(data))
+    .then((data) => changeDaysDataTemp(data));
 }
 
-async function changeDomData(data) {
-  // Data for each day
+function setWind(cityValue) {
+  getData(cityValue)
+    .then((data) => changeDisplayData(data))
+    .then((data) => changeDomDay(data))
+    .then((data) => changeDaysDataWind(data));
+}
+
+function setHum(cityValue) {
+  getData(cityValue)
+    .then((data) => changeDisplayData(data))
+    .then((data) => changeDomDay(data))
+    .then((data) => changeDaysDataHum(data));
+}
+function setPress(cityValue) {
+  getData(cityValue)
+    .then((data) => changeDisplayData(data))
+    .then((data) => changeDomDay(data))
+    .then((data) => changeDaysDataPress(data));
+}
+
+async function changeDaysDataTemp(data) {
+  // TEMP Data for each day
+
+  let daysSymbol = document.getElementsByClassName("symbol");
+  console.log(daysSymbol);
+
+  for (let i = 0; i < 7; i++) {
+    daysSymbol[i].textContent = " c";
+  }
 
   const dataOne = document.querySelector("#dataOne");
   dataOne.textContent = data.days[0].temp;
@@ -43,6 +98,112 @@ async function changeDomData(data) {
   const dataSeven = document.querySelector("#dataSeven");
   dataSeven.textContent = data.days[6].temp;
 
+  return data;
+}
+
+async function changeDaysDataWind(data) {
+  // WIND Data for each day
+
+  let daysSymbol = document.getElementsByClassName("symbol");
+  console.log(daysSymbol);
+
+  for (let i = 0; i < 7; i++) {
+    daysSymbol[i].textContent = " m/s";
+  }
+
+  const dataOne = document.querySelector("#dataOne");
+  dataOne.textContent = data.days[0].windspeed;
+
+  const dataTwo = document.querySelector("#dataTwo");
+  dataTwo.textContent = data.days[1].windspeed;
+
+  const dataThree = document.querySelector("#dataThree");
+  dataThree.textContent = data.days[2].windspeed;
+
+  const dataFour = document.querySelector("#dataFour");
+  dataFour.textContent = data.days[3].windspeed;
+
+  const dataFive = document.querySelector("#dataFive");
+  dataFive.textContent = data.days[4].windspeed;
+
+  const dataSix = document.querySelector("#dataSix");
+  dataSix.textContent = data.days[5].windspeed;
+
+  const dataSeven = document.querySelector("#dataSeven");
+  dataSeven.textContent = data.days[6].windspeed;
+
+  return data;
+}
+
+async function changeDaysDataHum(data) {
+  // WET Data for each day
+
+  let daysSymbol = document.getElementsByClassName("symbol");
+  console.log(daysSymbol);
+
+  for (let i = 0; i < 7; i++) {
+    daysSymbol[i].textContent = " %";
+  }
+
+  const dataOne = document.querySelector("#dataOne");
+  dataOne.textContent = data.days[0].humidity;
+
+  const dataTwo = document.querySelector("#dataTwo");
+  dataTwo.textContent = data.days[1].humidity;
+
+  const dataThree = document.querySelector("#dataThree");
+  dataThree.textContent = data.days[2].humidity;
+
+  const dataFour = document.querySelector("#dataFour");
+  dataFour.textContent = data.days[3].humidity;
+
+  const dataFive = document.querySelector("#dataFive");
+  dataFive.textContent = data.days[4].humidity;
+
+  const dataSix = document.querySelector("#dataSix");
+  dataSix.textContent = data.days[5].humidity;
+
+  const dataSeven = document.querySelector("#dataSeven");
+  dataSeven.textContent = data.days[6].humidity;
+
+  return data;
+}
+
+async function changeDaysDataPress(data) {
+  // PRESS Data for each day
+
+  let daysSymbol = document.getElementsByClassName("symbol");
+  console.log(daysSymbol);
+
+  for (let i = 0; i < 7; i++) {
+    daysSymbol[i].textContent = " hPa";
+  }
+
+  const dataOne = document.querySelector("#dataOne");
+  dataOne.textContent = data.days[0].pressure;
+
+  const dataTwo = document.querySelector("#dataTwo");
+  dataTwo.textContent = data.days[1].pressure;
+
+  const dataThree = document.querySelector("#dataThree");
+  dataThree.textContent = data.days[2].pressure;
+
+  const dataFour = document.querySelector("#dataFour");
+  dataFour.textContent = data.days[3].pressure;
+
+  const dataFive = document.querySelector("#dataFive");
+  dataFive.textContent = data.days[4].pressure;
+
+  const dataSix = document.querySelector("#dataSix");
+  dataSix.textContent = data.days[5].pressure;
+
+  const dataSeven = document.querySelector("#dataSeven");
+  dataSeven.textContent = data.days[6].pressure;
+
+  return data;
+}
+
+async function changeDisplayData(data) {
   // Data in the display
 
   const tempLow = document.querySelector("#tempLow");
@@ -124,4 +285,6 @@ async function changeDomDay(data) {
 
   const disHeader = document.querySelector("#disHeader");
   disHeader.textContent = "Today is: " + weekday[date.getDay()];
+
+  return data;
 }
